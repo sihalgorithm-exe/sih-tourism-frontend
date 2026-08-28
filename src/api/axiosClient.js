f4990d7c569c5ca2a4787c1989b9e9ea1b2a2776
Udith Polepeddi <pudpskk@gmail.com>
2026-08-28 18:27:11 +0530

    Made a change in axoisClient

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api';
+console.log('API BASE URL:', import.meta.env.VITE_API_BASE_URL);
 
 const axiosClient = axios.create({
   baseURL,
