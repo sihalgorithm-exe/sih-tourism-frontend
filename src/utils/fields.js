db4f108f1f57d4265f08844c374172a9ee27ac21
sihalgorithm-exe <sih.algorithm.exe@gmail.com>
2026-09-05 12:24:36 +0530

    Fixed image paths

--- src/utils/fields.js
+++ src/utils/fields.js
@@ -26,20 +26,23 @@ export function getDescription(obj) {
 }
 
 export function getImage(obj) {
-  const image = pick(obj, ['imgUrl', 'imageUrl', 'image', 'photoUrl', 'thumbnailUrl']);
+  const image = pick(obj, [
+    'imgUrl',
+    'imageUrl',
+    'image',
+    'photoUrl',
+    'thumbnailUrl'
+  ]);
 
   if (!image) return undefined;
 
+  // External image URL
   if (image.startsWith('http://') || image.startsWith('https://')) {
     return image;
   }
 
-  const apiBase = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api';
-
-  // Remove /api from the API base URL
-  const backendBase = apiBase.replace(/\/api\/?$/, '');
-
-  return `${backendBase}${image.startsWith('/') ? image : `/${image}`}`;
+  // Local frontend image
+  return image.startsWith('/') ? image : `/${image}`;
 }
 
 export function getLocation(obj) {
